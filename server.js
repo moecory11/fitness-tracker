dd0e62601447eda9b3d2361bad027739a8b866f0
Cory Moe <moecory11@gamil.com>
2021-05-14 16:48:55 -0700

    date and weight working in stats

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -5,6 +6,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
 const app = express();
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
